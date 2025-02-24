0f0bafc329e66a30a456b01b986f8acba1b1f7f0
Amar-Lenovo <amar.singh@silexsoftwares.com>
2025-02-24 17:33:52 +0530

    On scan add item to cart.

--- src/components/POSProductSearchBox.jsx
+++ src/components/POSProductSearchBox.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import db from "@/db/dexieDB";
 import { getProductDetail } from '@/utils/posService';
 import { updateSpecificProduct } from '@/utils/productService';
@@ -11,6 +11,7 @@ const POSProductSearchBox = ({ setSelectedProducts }) =>
   const { token, warehouseId } = useAuth();
   const [searchTerm, setSearchTerm] = useState("");
   const [products, setProducts] = useState([]);
+  const inputRef = useRef(null);
 
 
   useEffect(() =>
@@ -35,6 +36,8 @@ const POSProductSearchBox = ({ setSelectedProducts }) =>
     {
       const results = await db.products
         .where("name")
+        .startsWithIgnoreCase(query)
+        .or("sku")
         .startsWithIgnoreCase(query)
         //.and(obj => obj.in_stock == true)
         .toArray();
@@ -73,14 +76,58 @@ const POSProductSearchBox = ({ setSelectedProducts }) =>
     
   };
 
-
+  const addScannedProduct = (product) =>
+    {
+      
+      getProductDetail(token,product.id,warehouseId).then((obj_response)=>{
+        updateSpecificProduct(obj_response);
+        product=obj_response;
+        if(product.in_stock )
+        {
+          setSelectedProducts((prev) =>
+            prev.some((p) => p.id === product.id) ? prev : [...prev, product]
+          );
+        }
+        else{
+          toast.error("Product is out of stock", {
+                        position: "top-right",
+                        autoClose: 5000,
+                        toastId:`out_of_stock_${product.id}`
+                      });
+        }
+        
+        setProducts([]);
+        setSearchTerm('');
+      }).catch((obj_error)=>{
+            //Error case
+      })
+      
+    };
+  
+  // After SKU Scan scanner fire tab event and this function called and add items to cart.
+  const handleKeyDown = async (e) => {
+    if (e.key === "Enter" || e.key === "Tab") {
+      e.preventDefault();
+      const results = await db.products
+        .where("sku")
+        .equals(e.target.value.trim())
+        .toArray();
+      if(results.length > 0)
+      {
+        addScannedProduct(results[0]);
+        e.target.value="";
+      }
+    }
+  };
 
   return (
     <div>
       <input
         type="text"
+        ref={inputRef}
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={handleKeyDown}
         placeholder="Search products..."
         className="w-full px-2 py-2 border rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
       />
